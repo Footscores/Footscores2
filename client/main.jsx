23254a4b006a77f3e7401c5032813b0359ad09e4
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-27 00:49:14 -0500

    Add accounts configuration file

--- client/main.jsx
+++ client/main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {browserHistory, Router, Route, IndexRoute} from 'react-router';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import '../imports/startup/accounts-config.js';
 import App from '../imports/ui/App.js';
 import Home from '../imports/ui/home.js';
 import Leaderboard from '../imports/ui/leaderboard.js';
