9dc07e7804ec33b9be6e60ea4e1345824086a62a
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-27 10:16:41 -0500

    API calls work

---
+++ imports/api/methods.js
@@ -0,0 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { HTTP } from 'meteor/http';
+
+Meteor.methods({});
--- imports/startup/football-data-config.js
+++ imports/startup/football-data-config.js
@@ -2,8 +2,8 @@ module.exports = {
     hostname: 'https://api.football-data.org/v1/',
     options: {
       'headers': {
-        // 'X-Auth-Token': '58be930f357641e8a4cb1fecad504306',
-        // 'X-Response-Control': 'minified'
+        'X-Auth-Token': '58be930f357641e8a4cb1fecad504306',
+        'X-Response-Control': 'minified'
       }
     }
 }
