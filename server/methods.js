0e0f6f5cedc1e2193333f6f0eed6fea7dacebf29
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-03-30 16:38:33 -0500

    Add method to check a certain users guesses

--- server/methods.js
+++ server/methods.js
@@ -30,5 +30,19 @@ Meteor.methods({
   },
   'guesses.update'({id, correct}) {
     Guesses.update(id, {$set : {correct: correct} });
+  },
+  'guesses.check'({user, results}) {
+    var pending = Guesses.find({user}).fetch().filter((guess) => {return guess.correct == null || guess.correct == undefined});
+    for(var i=0; i<pending.length; i++) {
+      var guess = pending[i];
+      for (var j = 0; j<results.length; j++) {
+        if(pending[i].homeTeam === results[j].homeTeamName && pending[i].awayTeam === results[j].awayTeamName ) {
+          var isCorrect = ((pending[i].homeTeamScore === results[i].result.goalsHomeTeam) && (pending[i].awayTeamScore === results[j].result.goalsAwayTeam));
+          Guesses.update(pending[i]._id, {$set: {correct: isCorrect}});
+          if(isCorrect)
+            Meteor.users.udpate(user, { $inc:{score: 10, currentStreak: 1} });
+        }
+      }
+    }
   }
 });
