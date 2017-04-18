d9bfc335c75d443e5ac976b9638197fa3da86c36
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-04-18 16:18:37 -0500

    Move check method to server

--- imports/api/collections/guesses.js
+++ imports/api/collections/guesses.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
+import football from '../football-data.js';
 
 export const Guesses = new Mongo.Collection('guesses');
 
@@ -73,23 +74,5 @@ Meteor.methods({
   },
   'guesses.update'({id, correct}) {
     Guesses.update(id, {$set : {correct: correct} });
-  },
-  'guesses.check'({}) {
-    var user = this.userId;
-    var results = football.getMatchesDayBefore();
-    var pending = Guesses.find({user: user, correct: { $exists: false }}).fetch();
-    for(var i=0; i<pending.length; i++) {
-      var guess = pending[i];
-      for (var j = 0; j<results.length; j++) {
-        if(pending[i].homeTeam === results[j].homeTeamName && pending[i].awayTeam === results[j].awayTeamName) {
-          var isCorrect = ((pending[i].homeTeamScore === results[j].result.goalsHomeTeam) && (pending[i].awayTeamScore === results[j].result.goalsAwayTeam));
-          Guesses.update(pending[i]._id, {$set: {correct: isCorrect}});
-          if(isCorrect)
-            Meteor.users.update(user, { $inc:{"profile.score": 10, "profile.currentStreak": 1, "profile.longestStreak": 1} });
-          else
-            Meteor.users.update(user, { $set: {"profile.currentStreak": 0, "profile.longestStreak": 0} });
-        }
-      }
-    }
   }
 })
--- server/methods.js
+++ server/methods.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { HTTP } from 'meteor/http';
 import football from '../imports/api/football-data.js';
+import  Guesses  from '../imports/api/collections/guesses.js';
 
 Meteor.methods({
   'api.nextWeek'({}){
@@ -12,5 +13,29 @@ Meteor.methods({
   },
   'api.dayBefore'({}){
     return football.getMatchesDayBefore();
+  },
+  'guesses.check'({}) {
+    console.log('Checking the guesses of user ' + this.userId);
+    var user = this.userId;
+    var results = football.getMatchesDayBefore();
+    var pending = Guesses.find({user: user, correct: { $exists: false }}).fetch();
+    var total = pending.length;
+    var correct = 0;
+    for(var i=0; i<total; i++) {
+      var guess = pending[i];
+      for (var j = 0; j<results.length; j++) {
+        if(pending[i].homeTeam === results[j].homeTeamName && pending[i].awayTeam === results[j].awayTeamName) {
+          var isCorrect = ((pending[i].homeTeamScore === results[j].result.goalsHomeTeam) && (pending[i].awayTeamScore === results[j].result.goalsAwayTeam));
+          Guesses.update(pending[i]._id, {$set: {correct: isCorrect}});
+          if(isCorrect) {
+            Meteor.users.update(user, { $inc:{"profile.score": 10, "profile.currentStreak": 1, "profile.longestStreak": 1} });
+            correct++;
+          } else {
+              Meteor.users.update(user, { $set: {"profile.currentStreak": 0, "profile.longestStreak": 0} });
+          }
+        }
+      }
+    }
+    console.log('Found ' + correct + ' corect guesses out of ' + total + ' available to check.');
   }
 });
