0e9e3201a9f2089d5568003b35c1a076ca19d055
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-04 21:25:53 -0500

    Refactor startup code

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-plusplus, no-empty-pattern, object-shorthand, prefer-template,
+import/no-unresolved, no-underscore-dangle, import/extensions, import/no-extraneous-dependencies,
+import/no-named-as-default, no-undef
+*/
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.ui.config({
