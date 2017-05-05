c5840ed2f0bf5cd4209f1a7a6c2fdebcdf0fb088
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-04 22:05:05 -0500

    Refactor home and intento

---
+++ imports/ui/intento.jsx
@@ -0,0 +1,59 @@
+/* eslint-disable no-plusplus, no-empty-pattern, object-shorthand, prefer-template,
+import/no-unresolved, no-underscore-dangle, import/extensions, import/no-extraneous-dependencies,
+import/no-named-as-default, no-undef, react/prop-types, react/forbid-prop-types, arrow-body-style,
+react/require-default-props, no-else-return
+*/
+import React, { Component } from 'react';
+import '../style/App.css';
+
+class Intento extends Component {
+
+  renderResult() {
+    if (this.props.intento.correct !== undefined) {
+      if (this.props.intento.correct === true) {
+        return (<i className="fa fa-check-circle-o fa-lg" aria-hidden="true" />);
+      } else {
+        return (<i className="fa fa-times-circle fa-lg" aria-hidden="true" />);
+      }
+    } else {
+      return (<i className="fa fa-clock-o fa-lg" aria-hidden="true" />);
+    }
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12">
+
+          <div className="row">
+            <table className="table-matches">
+              <tbody>
+                <tr>
+                  <td className="col-md-4 col-xs-3 matches">{this.props.intento.homeTeam}</td>
+                  <td className="col-md-4 col-xs-6 matches">
+                    <div className="row">
+                      <div className="col-md-3 col-xs-0" />
+                      <div className="col-md-6">
+                        <div className="row result-guess">
+                          <span >
+                            {this.props.intento.homeTeamScore}-{this.props.intento.awayTeamScore}
+                          </span>
+                        </div>
+                        <div className="row">
+                          {this.renderResult()}
+                        </div>
+                      </div>
+                      <div className="col-md-3 col-xs-0" />
+                    </div>
+                  </td>
+                  <td className="col-md-4 col-xs-3 matches">{this.props.intento.awayTeam}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Intento;
