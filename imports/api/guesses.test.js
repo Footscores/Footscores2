b85e3d3df919f6ee04e6a48fc0ad469c2f07478d
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-05 11:03:40 -0500

    Minor fix

--- imports/api/guesses.test.js
+++ imports/api/guesses.test.js
@@ -17,6 +17,10 @@ if (Meteor.isServer) {
       beforeEach(() => {
         Guesses.remove({});
       });
+      it('Doesnt find when empty', () => {
+        // Verify that the method does what we expected
+        assert.equal(Guesses.find().count(), 0);
+      });
       it('Can create guess', () => {
         // Find the internal implementation of the insert guess method so we can
         // test it in isolation
@@ -31,6 +35,66 @@ if (Meteor.isServer) {
 
         // Verify that the method does what we expected
         assert.equal(Guesses.find().count(), 1);
+      });
+      it('Saved home team correctly', () => {
+        // Find the internal implementation of the insert guess method so we can
+        // test it in isolation
+        const addGuess = Meteor.server.method_handlers['guesses.insert'];
+        // console.log(Meteor.server);
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = { userId };
+
+        const date = Date.now();
+        // Run the method with `this` set to the fake invocation
+        addGuess.apply(invocation, [{ date: date, homeTeam: 'Leicester City FC', awayTeam: 'Sevilla FC', homeTeamScore: 2, awayTeamScore: 0 }]);
+
+        // Verify that the method does what we expected
+        assert.equal(Guesses.find().fetch()[0].homeTeam, 'Leicester City FC');
+      });
+      it('Saved away team correctly', () => {
+        // Find the internal implementation of the insert guess method so we can
+        // test it in isolation
+        const addGuess = Meteor.server.method_handlers['guesses.insert'];
+        // console.log(Meteor.server);
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = { userId };
+
+        const date = Date.now();
+        // Run the method with `this` set to the fake invocation
+        addGuess.apply(invocation, [{ date: date, homeTeam: 'Leicester City FC', awayTeam: 'Sevilla FC', homeTeamScore: 2, awayTeamScore: 0 }]);
+
+        // Verify that the method does what we expected
+        assert.equal(Guesses.find().fetch()[0].awayTeam, 'Sevilla FC');
+      });
+      it('Saved home team score correctly', () => {
+        // Find the internal implementation of the insert guess method so we can
+        // test it in isolation
+        const addGuess = Meteor.server.method_handlers['guesses.insert'];
+        // console.log(Meteor.server);
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = { userId };
+
+        const date = Date.now();
+        // Run the method with `this` set to the fake invocation
+        addGuess.apply(invocation, [{ date: date, homeTeam: 'Leicester City FC', awayTeam: 'Sevilla FC', homeTeamScore: 2, awayTeamScore: 0 }]);
+
+        // Verify that the method does what we expected
+        assert.equal(Guesses.find().fetch()[0].homeTeamScore, 2);
+      });
+      it('Saved away team score correctly', () => {
+        // Find the internal implementation of the insert guess method so we can
+        // test it in isolation
+        const addGuess = Meteor.server.method_handlers['guesses.insert'];
+        // console.log(Meteor.server);
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = { userId };
+
+        const date = Date.now();
+        // Run the method with `this` set to the fake invocation
+        addGuess.apply(invocation, [{ date: date, homeTeam: 'Leicester City FC', awayTeam: 'Sevilla FC', homeTeamScore: 2, awayTeamScore: 0 }]);
+
+        // Verify that the method does what we expected
+        assert.equal(Guesses.find().fetch()[0].awayTeamScore, 0);
       });
     });
   });
